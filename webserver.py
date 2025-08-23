72b1f7fe10c9bc5d043cb18458499ca9405fa68a
Aleeza Zahra <127841293+aleezazahra01@users.noreply.github.com>
2025-08-23 20:36:23 +0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,7 +7,8 @@ def home():
     return "discord bot ok"
 
 def run():
-    app.run(host='0.0.0.0', port=8080,debug=False)
+    port = int(os.environ.get("PORT", 8080))  # Railway requires PORT env
+    app.run(host="0.0.0.0", port=port)
 def keep_alive():
     t = Thread(target=run)
     t.start()   
@@ -15,4 +16,5 @@ def keep_alive():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False, threaded=False)
+
 
